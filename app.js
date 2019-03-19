f3f9edc34051cd9aa75c76fdda09668bce87fde9
Sayuj Sehgal <sayujsehgal@iprakash.com>
2019-03-19 21:08:05 +0530

    show page added

--- app.js
+++ app.js
@@ -61,6 +61,17 @@ app.post("/blogs", function(req, res){
     });
 });
 
+// SHOW ROUTE
+app.get("/blogs/:id", function(req, res){
+    Blog.findById(req.params.id, function(err, foundBlog){
+        if(err){
+            res.redirect("/blogs");
+        }
+        else{
+            res.render("show", {blog: foundBlog});
+        }
+    });
+});
 
 //Server Setup
 app.listen(3000, function(){
